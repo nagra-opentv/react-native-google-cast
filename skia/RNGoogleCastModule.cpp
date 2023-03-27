0786dbbe36d364ce89299b4a2e1d9b515e69966a
Kumar M Prasanna <prasanna.kumar@nagra.com>
2023-03-27 18:52:37 +0530

    Stubbing the methods of GoogleCast

---
+++ skia/RNGoogleCastModule.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 1994-2022 OpenTV, Inc. and Nagravision S.A.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "ReactSkia/utils/RnsLog.h"
+#include <cxxreact/JsArgumentHelpers.h>
+
+#include "RNGoogleCastModule.h"
+
+using namespace folly;
+namespace facebook {
+namespace xplat {
+
+RNGoogleCastModule::RNGoogleCastModule() { }
+
+auto RNGoogleCastModule::getConstants() -> std::map<std::string, folly::dynamic> {
+  return {};
+}
+
+std::string RNGoogleCastModule::getName() {
+  return "RNGoogleCast";
+}
+
+auto RNGoogleCastModule::getMethods() -> std::vector<Method> {
+  return {
+    Method(
+    "play",
+    [] (dynamic args){
+      RNS_LOG_NOT_IMPL;
+      return;
+      }), 
+ };
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+RNS_EXPORT_MODULE(RNGoogleCastModule)
+#ifdef __cplusplus
+}
+#endif
+
+} // xplat
+} // facebook
